9719336de1948fe56b754905b5dd462162a4b733
Najwan <najwanmuyassar16@gmail.com>
2026-09-07 10:37:12 +0700

    fix: clear config cache on serverless init

--- api/index.php
+++ api/index.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+$app->make(\Illuminate\Contracts\Console\Kernel::class)->call('config:clear');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
