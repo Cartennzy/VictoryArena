1745a2ac3f00e5a629b210e6294919d7680b8fd5
Najwan <najwanmuyassar16@gmail.com>
2026-09-07 14:35:28 +0700

    fix: set default cache and session driver

--- api/index.php
+++ api/index.php
@@ -1,17 +1,23 @@
 <?php
 
-// Force direktori cache Laravel ke /tmp (writable di Vercel)
+// Force log & cache configuration
 putenv('LOG_CHANNEL=stderr');
+putenv('CACHE_DRIVER=file');
+putenv('CACHE_STORE=file');
+putenv('SESSION_DRIVER=cookie');
+
+// Force direktori cache Laravel ke /tmp
 putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('APP_SERVICES_CACHE=/tmp/services.php');
 putenv('APP_CONFIG_CACHE=/tmp/config.php');
 putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 putenv('APP_EVENTS_CACHE=/tmp/events.php');
 
-// Siapkan struktur folder temporary untuk storage
+// Siapkan folder storage sementara
 $storagePaths = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
 ];
@@ -25,7 +31,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Pasang path storage ke /tmp
+// Gunakan /tmp sebagai root storage runtime
 $app->useStoragePath('/tmp/storage');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
